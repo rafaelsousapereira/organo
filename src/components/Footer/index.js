52bb8ae480f9c19b7329c854825517e0c7d20bba
rafaelofficial <rafael.sousa.pereira@mail.edu.usf.br>
2022-11-17 21:01:46 -0300

    fix: added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,36 @@
-import './Footer.css';
+import './Footer.css'
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <img src="/images/add.png" alt="Footer of page"/>
-        </footer>
-    );
+            <section>
+                <ul>
+                    <li>
+                        <a href="facebook.com" target="_blank">
+                            <img src="/images/fb.png" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="twitter.com" target="_blank">
+                            <img src="/images/tw.png" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="instagram.com" target="_blank">
+                            <img src="/images/ig.png" alt="" />
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src="/images/logo.png" alt="" />
+            </section>
+            <section>
+                <p>
+                    Desenvolvido por Rafael.
+                </p>
+            </section>
+    </footer>)
 }
 
 export default Footer;
