5fda0022d3a4a492c11560bf6742bfc1b4b0566e
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-01-19 23:18:47 -0300

    fix: refactor

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,21 +1,20 @@
 import './Team.css';
 import Card from '../Card';
 
-const Team = (props) => {
-  const { deleteAt } = props;
+const Team = ({ collaborators, name, secondaryColor, primaryColor, deleteAt }) => {
   return (
-    props.collaborators.length > 0 && (
-      <section className="team" style={{ backgroundColor: props.secondaryColor }}>
-        <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+    collaborators.length > 0 && (
+      <section className="team" style={{ backgroundColor: secondaryColor }}>
+        <h3 style={{ borderColor: primaryColor }}>{name}</h3>
         <div className="collaborator">
-          {props.collaborators.map((value) => {
+          {collaborators.map((value) => {
             return (
               <Card
                 key={value.collaborator}
                 collaborator={value.collaborator}
                 hole={value.hole}
                 image={value.image}
-                background={props.primaryColor}
+                background={primaryColor}
                 deleteAt={deleteAt}
               />
             );
